4aa29e54968b7571cbca7f0d37435cdba36eba80
MELL-K <k_vasiliy@mail.ru>
2026-02-01 15:50:07 +0300

    HW-10.1

--- src/Main.java
+++ src/Main.java
@@ -13,58 +13,55 @@ public class Main {
         int min = inputArray1[0];
         for (int i : inputArray1) {
             sum += i;
-        }
-        for (int i : inputArray1) {
-            if (max < i) {
+            if (i > max) {
                 max = i;
             }
-        }
-        for (int i : inputArray1) {
-            if (min > i) {
+            if (i < min) {
                 min = i;
             }
         }
-        float middle = (float) sum / inputArray1.length;
-        float[] outputArray1 = {sum, max, min, middle};
+        double middle = (double) sum / inputArray1.length;
+        double[] outputArray1 = {sum, max, min, middle};
         System.out.println(Arrays.toString(inputArray1));
         System.out.println(Arrays.toString(outputArray1));
+        System.out.println();
 
         System.out.println("Задача 2");
         int[] inputArray2 = {27000, 35000, 40000, 29000, 42000};
+        double[] outputArray2 = new double[inputArray2.length];
         int index = 0;
-        float[] outputArray2 = new float[5];
-        for (float pay : inputArray2) {
-            float tax = pay * 0.13f;
+        for (double pay : inputArray2) {
+            double tax = pay * 0.13;
             outputArray2[index] = tax;
             index++;
         }
         System.out.println(Arrays.toString(inputArray2));
         System.out.println(Arrays.toString(outputArray2));
+        System.out.println();
 
         System.out.println("Задача 3");
         int[] inPutArray3 = {4000, 3500, 4500, 7500, 5500};
-        boolean[] outPutArray3 = new boolean[5];
+        boolean[] outPutArray3 = new boolean[inPutArray3.length];
+        int maxbonus = 5000;
         for (int b = 0; b < inPutArray3.length; b++) {
-            if (inPutArray3[b] > 5000) {
-                outPutArray3[b] = true;
-            } else {
-                outPutArray3[b] = false;
-            }
-            System.out.println(inPutArray3[b]);
-            System.out.println(outPutArray3[b]);
+            outPutArray3[b] = inPutArray3[b] > maxbonus;
         }
+        System.out.println(Arrays.toString(inPutArray3));
+        System.out.println(Arrays.toString(outPutArray3));
+        System.out.println();
 
         System.out.println("Задача 4");
         int[] inputArray4 = {100, -200, 300, 400, -500};
-        boolean outputFlag = true;
-        for (int i = 0; i < inputArray4.length; i++) {
-            if (inputArray4[i] < 0) {
-                outputFlag = false;
+        boolean[] outputArray4 = {true};
+        for (int i : inputArray4) {
+            if (i < 0) {
+                outputArray4[0] = false;
                 break;
             }
         }
         System.out.println(Arrays.toString(inputArray4));
-        System.out.println(outputFlag);
+        System.out.println(Arrays.toString(outputArray4));
+        System.out.println();
 
         System.out.println("Задача 5");
         int[] inputArray5 = {15000, 2000, -1000, 12000, 10000};
@@ -76,5 +73,6 @@ public class Main {
         }
         System.out.println(Arrays.toString(inputArray5));
         System.out.println(Arrays.toString(outputArray5));
+        System.out.println();
     }
 }
